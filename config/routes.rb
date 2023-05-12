7586bd13ac65af7707aba868c73bfeb788f46d44
ghulamjilani <ghulamjilanideveloper@gmail.com>
2023-05-12 12:22:34 +0500

    API Created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  # namespace :api do
+  #   namespace :v1 do
+  #     get 'products/index'
+  #     get 'products/show'
+  #   end
+  # end
+
+  namespace :api do
+    namespace :v1 do
+      resources :products, only: [:index, :create, :show]
+    end
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.create([
+    {
+    name: "Quiet Comfort 35",
+    brand: "Bose",
+    price: "$279.99",
+    description: "Wireless Bluetooth Headphones, Noise-Cancelling, with Alexa voice control - Black"
+    },
+    {
+    name: "Noise Cancelling Headphones WH1000XM3",
+    brand: "Sony",
+    price: "$348.00",
+    description: "Wireless Bluetooth Over the Ear Headset with Mic for phone-call and Alexa voice control - Industry Leading Active Noise Cancellation - Black"
+    },
+    {
+    name: "Freedom Aluminum Patio Panel Sliding Glass Pet Door", 
+    brand: "PetSafe",
+    price: "$124.95",
+    description: "Sliding Glass Pet Door for Dogs and Cats - Adjustable Frame."
+    },
+    {
+    name: "Quadcopters Mini Drone",
+    brand: "Crazepony",
+    price: "$88.79",
+    description: "Crazepony Quad Rotor Open Source PCB Development Platform for Student Maker Geeker"
+    },
+    {
+    name: "CJ890 Series Desktop Curved Monitor",
+    brand: "Samsung",
+    price: "$1099.79",
+    description: "49 inch 3840x1080 Super Ultra-Wide Desktop Monitor for Business, 144 Hz, USB-C, HDMI, DisplayPort, 3-Year Warranty (C493898DKN)"
+    }
+])
